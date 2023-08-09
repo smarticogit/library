b1b2bbb60248551e3143945e95d6297fd32f342b
Daniel <daniel.repres@hotmail.com>
2023-08-09 15:56:14 -0300

    refactor

---
+++ src/test/java/com/domvs/library/service/ImplLibraryTest.java
@@ -0,0 +1,34 @@
+package com.domvs.library.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ImplLibraryTest {
+
+    @Test
+    void saveBook() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void toLoan() {
+    }
+
+    @Test
+    void giveBack() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
